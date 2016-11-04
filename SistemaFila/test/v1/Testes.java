90c48c7434683b39cc43aeea7a19df3e9d3401f3
Renzo <renzo@localhost.localdomain>
2016-11-03 22:00:59 -0200

    alterações no branch v2

--- SistemaFila/test/v1/Testes.java
+++ SistemaFila/test/v1/Testes.java
@@ -9,9 +9,9 @@ public class Testes {
 
 	@Test
 	public void exemplo() {
-		Pessoa p1 = new Pessoa("Fulano", false, false);
-		Pessoa p2 = new Pessoa("Ciclana", false, false);
-		Pessoa p3 = new Pessoa("Beltrana", false, false);
+		Pessoa p1 = new Pessoa("Renzo", false, false);
+		Pessoa p2 = new Pessoa("Fulana", false, false);
+		Pessoa p3 = new Pessoa("Ciclana", false, false);
 		
 		Fila f = new Fila();
 		
@@ -21,17 +21,75 @@ public class Testes {
 		
 		Pessoa proximo = f.proximoFila();
 		
-		Assert.assertEquals(proximo.getNome(), "Fulano");
+		Assert.assertEquals(proximo.getNome(), "Renzo");
 		
 		f.removePessoa(proximo);
 		proximo = f.proximoFila();
 			
-		Assert.assertEquals(proximo.getNome(), "Ciclana");
+		Assert.assertEquals(proximo.getNome(), "Fulana");
 		
 		f.removePessoa(proximo);
 		proximo = f.proximoFila();
 		
-		Assert.assertEquals(proximo.getNome(), "Beltrana");
+		Assert.assertEquals(proximo.getNome(), "Ciclana");
+		
+		f.removePessoa(proximo);
+	}
+
+	@Test
+	public void testeGravida() {
+		Pessoa p1 = new Pessoa("Renzo", false, false);
+		Pessoa p2 = new Pessoa("Fulana", true, false);
+		Pessoa p3 = new Pessoa("Ciclana", false, false);
+		
+		Fila f = new Fila();
+		
+		f.addPessoa(p1);
+		f.addPessoa(p2);
+		f.addPessoa(p3);
+		
+		Pessoa proximo = f.proximoFila();
+		
+		Assert.assertEquals(proximo.getNome(), "Fulana");
+		
+		f.removePessoa(proximo);
+		proximo = f.proximoFila();
+			
+		Assert.assertEquals(proximo.getNome(), "Renzo");
+		
+		f.removePessoa(proximo);
+		proximo = f.proximoFila();
+		
+		Assert.assertEquals(proximo.getNome(), "Ciclana");
+		
+		f.removePessoa(proximo);
+	}
+	
+	@Test
+	public void testeIdoso() {
+		Pessoa p1 = new Pessoa("Renzo", false, false);
+		Pessoa p2 = new Pessoa("Fulana", true, false);
+		Pessoa p3 = new Pessoa("Ciclana", false, true);
+		
+		Fila f = new Fila();
+		
+		f.addPessoa(p1);
+		f.addPessoa(p2);
+		f.addPessoa(p3);
+		
+		Pessoa proximo = f.proximoFila();
+		
+		Assert.assertEquals(proximo.getNome(), "Ciclana");
+		
+		f.removePessoa(proximo);
+		proximo = f.proximoFila();
+			
+		Assert.assertEquals(proximo.getNome(), "Fulana");
+		
+		f.removePessoa(proximo);
+		proximo = f.proximoFila();
+		
+		Assert.assertEquals(proximo.getNome(), "Renzo");
 		
 		f.removePessoa(proximo);
 	}
